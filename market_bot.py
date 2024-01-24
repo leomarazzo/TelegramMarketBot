b132fd3575dd0bb6ea668f6f9cbcfaa079932e9a
Leonardo Marazzo <leonardojmarazzo@gmail.com>
2024-01-24 18:00:48 -0300

    Add messages to json

--- market_bot.py
+++ market_bot.py
@@ -18,7 +18,7 @@ class MarketBot:
 
     def start_new_list(self, message):
         self.lists[message.chat.id] = MarketList(message.chat.id, [])
-        self.bot.send_message(message.chat.id, f"New list created for: {message.from_user.username}")
+        self.bot.send_message(message.chat.id, self.messages["new list"]["en"].format(message.from_user.username))
     
     def add_to_list(self, message):
         if message.chat.id in self.lists:
@@ -26,26 +26,26 @@ class MarketBot:
             self.bot.reply_to(message, "Added")
             self.bot.send_message(message.chat.id, f"Total: {self.lists[message.chat.id].total()}")
         else:
-            self.bot.send_message(message.chat.id, f"No list created for: {message.from_user.username}.\nUse the /new command to create a new list")
+            self.bot.send_message(message.chat.id, self.messages["no list"]["en"].format(message.from_user.username))
 
     def print(self, message):
         if message.chat.id in self.lists:
             self.bot.send_message(message.chat.id, self.lists[message.chat.id].serialize(), parse_mode="MarkdownV2")
             self.bot.send_message(message.chat.id, "If you need more details you can use the /print_det command (Probably you will need a wider screen, you can rotate your phone for this).")
         else:
-            self.bot.send_message(message.chat.id, f"No list created for: {message.from_user.username}.\nUse the /new command to create a new list")
+            self.bot.send_message(message.chat.id, self.messages["no list"]["en"].format(message.from_user.username))
     
     def print_det(self, message):
         if message.chat.id in self.lists:
             self.bot.send_message(message.chat.id, self.lists[message.chat.id].serialize_detailed(), parse_mode="MarkdownV2")
         else:
-            self.bot.send_message(message.chat.id, f"No list created for: {message.from_user.username}.\nUse the /new command to create a new list")
+            self.bot.send_message(message.chat.id, self.messages["no list"]["en"].format(message.from_user.username))
 
     def total(self, message):
         if message.chat.id in self.lists:
             self.bot.send_message(message.chat.id, self.lists[message.chat.id].total())
         else:
-            self.bot.send_message(message.chat.id, f"No list created for: {message.from_user.username}.\nUse the /new command to create a new list")
+            self.bot.send_message(message.chat.id, self.messages["no list"]["en"].format(message.from_user.username))
 
     def export(self, message):
         if message.chat.id in self.lists:
@@ -56,7 +56,7 @@ class MarketBot:
                 doc = open(path, 'rb')
                 self.bot.send_document(message.chat.id, doc)
         else:
-            self.bot.send_message(message.chat.id, f"No list created for: {message.from_user.username}.\nUse the /new command to create a new list")
+            self.bot.send_message(message.chat.id, self.messages["no list"]["en"].format(message.from_user.username))
 
     def register_handlers(self):
         self.bot.register_message_handler(self.send_welcome, commands=['start', 'help'])
